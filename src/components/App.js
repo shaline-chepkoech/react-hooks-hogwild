9921a0b12ad7d84c1529c897552c25e48c4fff01
shaline-chepkoech <chepkoechshaline726@gmail.com>
2024-11-02 22:09:04 +0300

    c2

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import hogs from "../porkers_data";
 function App() {
 	const [selectedHog, setSelectedHog] = useState(null);
 	const [showGreasedOnly, setShowGreasedOnly] = useState(false);
+	const [sortCriterion, setSortCriterion] = useState("name");
 
 	const handleHogClick = (hog) => {
 		setSelectedHog(hog);
@@ -19,7 +20,19 @@ function App() {
 		setShowGreasedOnly((prev) => !prev);
 	};
 
+	const handleSortChange = (e) => {
+		setSortCriterion(e.target.value);
+	};
+
 	const filteredHogs = showGreasedOnly ? hogs.filter((hog) => hog.greased) : hogs;
+    const sortedHogs = [...filteredHogs].sort((a, b) => {
+		if (sortCriterion === "name") {
+			return a.name.localeCompare(b.name);
+		} else if (sortCriterion === "weight") {
+			return a.weight - b.weight;
+		}
+		return 0;
+	});
 
 	return (
 		<div className="App">
@@ -34,8 +47,18 @@ function App() {
 					Show Greased Hogs Only
 				</label>
 			</div>
+			
+			<div className="sort-container">
+			<label>
+					Sort by: 
+					<select value={sortCriterion} onChange={handleSortChange}>
+						<option value="name">Name</option>
+						<option value="weight">Weight</option>
+					</select>
+				</label>
+			</div>
 			<div className="hog-container">
-			{filteredHogs.map((hog) => (
+			{sortedHogs.map((hog) => (
 					<div className="hog-tile" key={hog.name} onClick={() => handleHogClick(hog)}>
 						<h3>{hog.name}</h3>
 						<img src={hog.image} alt={hog.name} />
